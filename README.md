f817e05bc5e082c53ab46f2e08d5c22ef6749b78
marckwei <xiangwei@tju.edu.cn>
2023-03-01 13:52:26 +0800

    found excel crash

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # PocGetter
 A script for extracting the latest security-related samples from  Webkit stress test set
 # Usage
+## JavascriptCore
 cd WebKit<br>
-git log --date=short -60000 | python main.py<br>
+git log --date=short -60000 | python ..\pocgetter\process_webkit.py<br>
+cd Webkit<br>
 python extract.py
+
+## V8
+cd v8<br>
+git log --date=short --name-status -60000 | python ..\pocgetter\process_v8.py
+
+## Spidermonkey
+cd gecko-dev/js
+git log --date=short --name-status . | python ..\pocgetter\process_spidermonkey.py
--- extract.py
+++ extract.py
@@ -16,38 +16,85 @@ def mkdir(path):
 		print("---  There is this folder!  ---")
 
 date = datetime.date.today().strftime('%Y-%m-%d')
-csvpath = r"D:\workspace\WebKit\gitlog.csv"
-outpath =r"D:\workspace\newpoc\webkit" +'\\' + date
+csvpath = r"D:\workspace\WebKit\v8-2023-2-27.csv"
+outpath =r"D:\workspace\newpoc\v8" +'\\' + date
 folder = mkdir(outpath)
 
-pf=pd.read_csv(csvpath,usecols=['hash','poc','date'],nrows=4000,squeeze=True)
+#pf=pd.read_csv(csvpath,usecols=['hash','poc','date'],nrows=4000,squeeze=True)
+pf=pd.read_csv(csvpath,usecols=['hash','poc','date'],squeeze=True)
 data=pf.dropna().values.tolist()
+print(data)
 
-
-def extrac_poc(export):
+def extrac_webkit_poc(export):
     num=0
+    first=""
+    last=""
     temp=0
     for f in export:
-        poc = re.compile('([\w/-]*.js)').match(f[2])
-        if poc:
-            fn = re.compile('[\w-]*(?=.js)').search(poc.group(1))
-            if fn:
-                fnn = fn.group(0)+'.js'
-                try:
-                    shutil.copy(poc.group(1),outpath+'\\'+fnn)
-                except FileNotFoundError:
-                    print("------------------------------------------------------------")
-                    print("Not Found!".center(60,' '))
-                    print(poc.group(1).center(60, ' '))
-                    print(("commit:%s"%(f[1])).center(60,' '))
-                    print("------------------------------------------------------------")
-                else:
-                    num+=1
-                    temp=f[2]
-                    if(num==1): print("The date of first poc:",f[0])
-                    #print(fnn)
-    print("The date of last poc:",f[0])   
+        poc = re.compile('([\w/-]+.js)').findall(f[2])
+        if len(poc)>0:
+            for i in poc:
+                fn = re.compile('[\w-]+(?=.js)').search(i)
+                # print(fn)
+                # print(i)
+                if fn:
+                    fnn = f[0]+f[1][0:5]+fn.group(0)+'.js'
+                    try:
+                        shutil.copy(i,outpath+'\\'+fnn)
+                    except FileNotFoundError:
+                        print(fn.group(0))
+                        # print("------------------------------------------------------------")
+                        # print("Not Found!".center(60,' '))
+                        # print(i.center(60, ' '))
+                        # print(("commit:%s"%(f[1])).center(60,' '))
+                        # print("------------------------------------------------------------")
+
+                    else:
+                        num+=1
+                        temp=f[2]
+                        last= f[0]
+                        if(num==1): first = f[0]
+                        #print(fnn)
+    print("The date of first poc:",first) 
+    print("The date of last poc:",last)   
+    print("Number of poc: ",num)
+def extrac_v8_poc(export):
+    num=0
+    first=""
+    last=""
+    temp=0
+    for f in export:
+        poc = re.compile('([\w/-]+.js)').findall(f[2])
+        if len(poc)>0:
+            for i in poc:
+                fn = re.compile('[\w-]+(?=.js)').search(i)
+                # print(fn)
+                # print(i)
+                if fn:
+                    if bool(re.match('regress', fn.group(0), re.IGNORECASE)):
+                        fnn = f[0] + f[1][0:5] + fn.group(0) + '.js'
+
+                        try:
+                            shutil.copy(i,outpath+'\\'+fnn)
+                        except FileNotFoundError:
+                            print(fn.group(0))
+                            # print("------------------------------------------------------------")
+                            # print("Not Found!".center(60,' '))
+                            # print(i.center(60, ' '))
+                            # print(("commit:%s"%(f[1])).center(60,' '))
+                            # print("------------------------------------------------------------")
+
+                        else:
+                            num+=1
+                            temp=f[2]
+                            last= f[0]
+                            if(num==1): first = f[0]
+                        #print(fnn)
+                    else:
+                        pass
+    print("The date of first poc:",first) 
+    print("The date of last poc:",last)   
     print("Number of poc: ",num)
 if __name__ == '__main__':
-    extrac_poc(data)
+    extrac_v8_poc(data)
     #print(data)
